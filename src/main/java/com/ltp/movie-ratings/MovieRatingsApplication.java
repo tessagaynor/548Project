fb23d4bad7cce84d41a617a3d06113a301e0fd7e
ellabrastad <emb59519@creighton.edu>
2023-11-17 17:52:02 -0600

    controller and application complete

--- src/main/java/com/ltp/movie-ratings/MovieRatingsApplication.java
+++ src/main/java/com/ltp/movie-ratings/MovieRatingsApplication.java
@@ -1,5 +1,10 @@
 package java.com.ltp.movie-ratings;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 public class MovieRatingsApplication {
-    
+    public static void main(String[] args) {
+		SpringApplication.run(MovieRatingsApplication.class, args);
+}
 }
