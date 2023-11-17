d8e5aeb3c5ff1784ce1a4666e07321a209bb3b40
Tessa Gaynor <tessagaynorr@gmail.com>
2023-11-17 17:44:02 -0600

    Constants and Movie classes

--- src/main/java/com/ltp/movie-ratings/Movie.java
+++ src/main/java/com/ltp/movie-ratings/Movie.java
@@ -1,5 +1,61 @@
 package java.com.ltp.movie-ratings;
 
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class Movie {
-    
+    private String genre;
+    private String name;
+    private String rating;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private String id;
+
+
+    public Movie() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
