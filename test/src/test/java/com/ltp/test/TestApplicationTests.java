0878123b14a60189f78ff93894f2d85baf7cf50e
ellabrastad <emb59519@creighton.edu>
2023-11-27 18:54:50 -0600

    edits

--- test/src/test/java/com/ltp/test/TestApplicationTests.java
+++ test/src/test/java/com/ltp/test/TestApplicationTests.java
@@ -2,7 +2,7 @@ package com.ltp.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-/*comment */
+
 @SpringBootTest
 class TestApplicationTests {
 
